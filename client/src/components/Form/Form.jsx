16692ab5f3364319d00f47ee52b55f76e68879c2
MaryAnn24 <maryann.baricante24@gmail.com>
2022-10-21 11:47:19 +0400

    Update

---
+++ client/src/components/Form/Form.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Form() {
+  return (
+    <div>
+        <h1>Start with the multi-form</h1>
+        <p>Select Jurisdiction</p>
+    </div>
+  )
+}
+
+export default Form
